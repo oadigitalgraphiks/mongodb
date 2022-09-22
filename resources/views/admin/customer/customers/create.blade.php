4271c678d01dc31dc4fa55b1922341a4bfd8f32d
Raza-DG <ra@digitalgraphiks.pk>
2022-09-22 12:52:55 +0500

    Customer Done

--- resources/views/admin/customer/customers/create.blade.php
+++ resources/views/admin/customer/customers/create.blade.php
@@ -2,105 +2,60 @@
 
 @section('content')
 
-<!--begin::Content-->
 <div class="d-flex flex-column flex-column-fluid" id="kt_content">
-    <!--begin::Toolbar-->
     <div class="toolbar" id="kt_toolbar">
-        <!--begin::Container-->
         <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
-            <!--begin::Page title-->
             <div data-kt-swapper="true" data-kt-swapper-mode="prepend"
                 data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
                 class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
-                <!--begin::Title-->
                 <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">Customers</h1>
-                <!--end::Title-->
-                <!--begin::Separator-->
                 <span class="h-20px border-gray-300 border-start mx-4"></span>
-                <!--end::Separator-->
-                <!--begin::Breadcrumb-->
                 <ul class="breadcrumb breadcrumb-separatorless fw-bold fs-7 my-1">
-                    <!--begin::Item-->
                     <li class="breadcrumb-item text-muted">
                         <a href="{{ route("admin.dashboard") }}" class="text-muted text-hover-primary">Home</a>
                     </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
                     <li class="breadcrumb-item">
                         <span class="bullet bg-gray-300 w-5px h-2px"></span>
                     </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item text-muted">eCommerce</li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
+                    <li class="breadcrumb-item text-dark">Customers</li>
                     <li class="breadcrumb-item">
                         <span class="bullet bg-gray-300 w-5px h-2px"></span>
                     </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item text-muted">Catalog</li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item">
-                        <span class="bullet bg-gray-300 w-5px h-2px"></span>
-                    </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
                     <li class="breadcrumb-item text-dark">Customer Add</li>
-                    <!--end::Item-->
                 </ul>
-                <!--end::Breadcrumb-->
             </div>
-            <!--end::Page title-->
         </div>
-        <!--end::Container-->
     </div>
-    <!--end::Toolbar-->
     <div class="post d-flex flex-column-fluid" id="kt_post">
-        <!--begin::Container-->
         <div id="kt_content_container" class="container-xxl ">
-            <!--begin::Post-->
             <form class="form d-flex flex-column flex-lg-row gap-7 gap-lg-10"
                 action="{{ route('admin.customers.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <!--begin::Main column-->
                 <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10">
-                    <!--begin::General options-->
                     <div class="card card-flush py-4">
-                        <!--begin::Card header-->
                         <div class="card-header">
                             <div class="card-title">
                                 <h2>{{translate('Customer')}}</h2>
                             </div>
                         </div>
-                        <!--end::Card header-->
-                        <!--begin::Card body-->
                         <div class="card-body pt-0 row">
-                            <!--begin::Input group-->
                             <div class="mb-5 fv-row col-6">
-                                <label class="required form-label">{{ translate('First Name') }}</label>
-                                <input type="text" placeholder="{{ translate('First Name') }}" id="first_name" name="first_name"
+                                <label class="required form-label">{{ translate('Name') }}</label>
+                                <input type="text" placeholder="{{ translate('Name') }}" id="name" name="name"
                                     class="form-control mb-2" required>
-                            </div>
-                            <div class="mb-5 fv-row col-6">
-                                <label class="required form-label">{{ translate('Last Name') }}</label>
-                                <input type="text" placeholder="{{ translate('Name') }}" id="last_name" name="last_name"
-                                    class="form-control mb-2" required>
-                            </div>
-
-                            <div class="mb-5 fv-row col-6">
-                                <label class="required form-label">{{ translate('User Name') }}</label>
-                                <input type="text" placeholder="{{ translate('User Name') }}" id="user_name" name="user_name"
-                                    class="form-control mb-2" required>
+                                @if($errors->has('name'))
+                                    <div class="error  text-danger">{{ $errors->first('name') }}</div>
+                                @endif
                             </div>
 
                             <div class="mb-5 fv-row col-6">
                                 <label class="required form-label">{{ translate('Email') }}</label>
                                 <input type="email" placeholder="{{ translate('Email') }}" id="email" name="email"
                                     class="form-control mb-2" required>
-                                <div class="text-muted fs-7">{{translate('A Email must be an Unique')}}.
-                                </div>
+                                <div class="text-muted fs-7">{{translate('A Email must be an Unique')}}.</div>
+                                @if($errors->has('email'))
+                                    <div class="error  text-danger">{{ $errors->first('email') }}</div>
+                                @endif
                             </div>
 
                             <div class="mb-5 fv-row col-6">
@@ -108,52 +63,9 @@
                                 <input type="password" placeholder="{{ translate('Password') }}" id="password" name="password" class="form-control mb-2" required>
                             </div>
 
-                            <!--begin::Input group-->
                         </div>
-                        <!--end::Card header-->
                     </div>
 
-                    <!--begin::Media-->
-                    <div class="card card-flush py-4">
-                        <!--begin::Card header-->
-                        <div class="card-header">
-                            <div class="card-title">
-                                <h3>{{translate('Media')}}</h3>
-                            </div>
-                        </div>
-                        <!--end::Card header-->
-                        <!--begin::Card body-->
-                        <div class="card-body pt-0">
-                            <!--begin::Input group-->
-                            <div class="fv-row mb-2">
-                                <label class="form-label">{{ translate('User Profile') }}</label>
-                                <!--begin::Dropzone-->
-                                <div class="dropzone" id="kt_ecommerce_add_product_mediaa"
-                                    data-toggle="aizuploader" data-type="image">
-                                    <!--begin::Message-->
-                                    <div class="dz-message needsclick">
-                                        <!--begin::Icon-->
-                                        <i class="bi bi-file-earmark-arrow-up text-primary fs-3x"></i>
-                                        <!--end::Icon-->
-                                        <!--begin::Info-->
-                                        <input type="hidden" name="image" class="selected-files">
-                                        <div class="ms-4">
-                                            <span class="fs-7 fw-bold text-gray-400">{{translate('User Logo')}}</span>
-                                        </div>
-                                        <!--end::Info-->
-                                    </div>
-                                </div>
-                                <!--end::Dropzone-->
-                                <div class="file-preview box sm">
-                                </div>
-
-
-                            </div>
-                        </div>
-                        <!--end::Card header-->
-                    </div>
-                    <!--end::Media-->
-                    <!--end::General options-->
                     <div class="d-flex justify-content-end">
 
                         <button type="submit" id="kt_ecommerce_add_category_submit" class="btn btn-primary">
@@ -161,83 +73,10 @@
                             <span class="indicator-progress">Please wait...
                                 <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                         </button>
-                        <!--end::Button-->
                     </div>
-                </div>
-                <!--end::Main column-->
-            </form>
-        </div>
-        <!--end::Container-->
-    </div>
-</div>
-
-
-
-{{-- <div class="aiz-titlebar text-left mt-2 mb-3">
-    <h5 class="mb-0 h6">{{translate('Brand Information')}}</h5>
-</div>
-
-<div class="col-lg-8 mx-auto">
-    <div class="card">
-        <div class="card-body p-0">
-            <ul class="nav nav-tabs nav-fill border-light">
-  				@foreach (\App\Models\Language::all() as $key => $language)
-  					<li class="nav-item">
-  						<a class="nav-link text-reset @if ($language->code == $lang) active @else bg-soft-dark border-light border-left-0 @endif py-3" href="{{ route('brands.edit', ['id'=>$brand->id, 'lang'=> $language->code] ) }}">
-  							<img src="{{ static_asset('assets/img/flags/'.$language->code.'.png') }}" height="11" class="mr-1">
-  							<span>{{ $language->name }}</span>
-  						</a>
-  					</li>
-	            @endforeach
-  			</ul>
-            <form class="p-4" action="{{ route('brands.update', $brand->id) }}" method="POST" enctype="multipart/form-data">
-                <input name="_method" type="hidden" value="PATCH">
-                <input type="hidden" name="lang" value="{{ $lang }}">
-                @csrf
-                <div class="form-group row">
-                    <label class="col-sm-3 col-from-label" for="name">{{translate('Name')}} <i class="las la-language text-danger" title="{{translate('Translatable')}}"></i></label>
-                    <div class="col-sm-9">
-                        <input type="text" placeholder="{{translate('Name')}}" id="name" name="name" value="{{ $brand->getTranslation('name', $lang) }}" class="form-control" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-3 col-form-label" for="signinSrEmail">{{translate('Logo')}} <small>({{ translate('120x80') }})</small></label>
-                    <div class="col-md-9">
-                        <div class="input-group" data-toggle="aizuploader" data-type="image">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text bg-soft-secondary font-weight-medium">{{ translate('Browse')}}</div>
-                            </div>
-                            <div class="form-control file-amount">{{ translate('Choose File') }}</div>
-                            <input type="hidden" name="logo" value="{{$brand->logo}}" class="selected-files">
-                        </div>
-                        <div class="file-preview box sm">
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-3 col-from-label">{{translate('Meta Title')}}</label>
-                    <div class="col-sm-9">
-                        <input type="text" class="form-control" name="meta_title" value="{{ $brand->meta_title }}" placeholder="{{translate('Meta Title')}}">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-3 col-from-label">{{translate('Meta Description')}}</label>
-                    <div class="col-sm-9">
-                        <textarea name="meta_description" rows="8" class="form-control">{{ $brand->meta_description }}</textarea>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-3 col-from-label" for="name">{{translate('Slug')}}</label>
-                    <div class="col-sm-9">
-                        <input type="text" placeholder="{{translate('Slug')}}" id="slug" name="slug" value="{{ $brand->slug }}" class="form-control">
-                    </div>
-                </div>
-                <div class="form-group mb-0 text-right">
-                    <button type="submit" class="btn btn-primary">{{translate('Save')}}</button>
                 </div>
             </form>
         </div>
     </div>
-</div> --}}
-
+</div>
 @endsection
